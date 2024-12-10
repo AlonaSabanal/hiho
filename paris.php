15ce177dca6221c45e4ee97343ed135611712395
unknown <mara.sabanal.coc@phinmaed.com>
2024-12-10 20:31:29 +0800

    uploaded modular method example code

---
+++ paris.php
@@ -0,0 +1,5 @@
+
+    <h1>Paris</h1>
+    <p>Paris is the capital city of France. Paris was the largest city of the France, with a metropolitan area of over 13 million inhabitants.</p>
+    <p>For centuries, Paris has attracted artists from around the world. As a result, Paris has acquired a reputation as the "City of Art"</p>
+
---
+++ tokyo.php
@@ -0,0 +1,5 @@
+
+    <h1>Tokyo</h1>
+    <p>Tokyo is the capital city of Japan. Throughout history, the national capital has been in locations other than Tokyo. The oldest capital of Japan is Nara.</p>
+    <p>After 1868, the seat of the Government of Japan and the location of the Emperor's home was moved to Edo, which it renamed Tokyo.</p>
+
